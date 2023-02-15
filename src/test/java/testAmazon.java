80dc0e5a72f90ae8a6a8e34f933eaa1ccb1e68c3
Arzu Bashirova <arzubashirova17@gmail.com>
2023-02-15 14:52:04 -0500

    committing the codes

---
+++ src/test/java/testAmazon.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testAmazon {
+
+    public static void main(String[] args) {
+
+
+        WebDriver driver= new ChromeDriver();
+
+        driver.get("https://www.amazon.com/");
+
+
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+
+        String searchTerm ="kids toys";
+        String expectedTitle="Amazon.com : "+ searchTerm;
+
+        searchBox.sendKeys(searchTerm, Keys.ENTER);
+
+        String actualTitle = driver.getTitle();
+
+        if(actualTitle.equalsIgnoreCase(expectedTitle)){
+
+            System.out.println("PASSED.");
+        }else{
+            System.out.println("Test Failed. The current Title is "+actualTitle);
+        }
+    }
+}
